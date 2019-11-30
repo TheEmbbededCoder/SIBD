1b69e65701d1ca5bcfe9ec3183861ebab66190d4
andrefrmeneses <45438041+andrefrmeneses@users.noreply.github.com>
2019-11-30 22:37:13 +0000

    Add files via upload

--- add_procedure_charting.php
+++ add_procedure_charting.php
@@ -1,7 +1,7 @@
 <html>
 <head>
 	<?php
-	echo("<title>Add procedure charting Information</title>");
+	echo("<title>Procedure charting Information</title>");
 		
 	?>
 	<script>
@@ -15,7 +15,7 @@
 </head>
 <body>
 	<?php
-	echo("<h1>Add procedure charting Information</h1>");
+	echo("<h1>Procedure charting Information</h1>");
 	$host="db.ist.utl.pt";
 	$user="ist425355";  
 	$password="emyg3992";
@@ -73,147 +73,139 @@
 			}
 		}
 
-		echo("<p>Will be inserted a procedure charting.</p>");
-
-		echo("<p>Success !</p>");
-
 	}
-	else{
+	// Show the received client
+	echo("<p><b>Client: </b>");
+	// Gets the VAT of the selected client
+	if(isset($_REQUEST['VAT_client'])) {
+		$VAT_client = $_REQUEST['VAT_client'];
+		echo("VAT: ");
+		echo($VAT_client);
+	}
+	// Gets the VAT of the selected client
+	if(isset($_REQUEST['Client_Name'])) {
+		$Client_Name = $_REQUEST['Client_Name'];
+		echo(" - Name: ");
+		echo($Client_Name);
+	}
+	echo("</p>");
 
-		// Show the received client
-		echo("<p><b>Client: </b>");
-		// Gets the VAT of the selected client
-		if(isset($_REQUEST['VAT_client'])) {
-			$VAT_client = $_REQUEST['VAT_client'];
-			echo("VAT: ");
-			echo($VAT_client);
-		}
-		// Gets the VAT of the selected client
-		if(isset($_REQUEST['Client_Name'])) {
-			$Client_Name = $_REQUEST['Client_Name'];
-			echo(" - Name: ");
-			echo($Client_Name);
-		}
-		echo("</p>");
+	// Show the received doctor
+	echo("<p><b>Doctor: </b>");
+	// Gets the VAT of the selected client
+	if(isset($_REQUEST['VAT_doctor'])) {
+		$VAT_doctor = $_REQUEST['VAT_doctor'];
+		echo("VAT: ");
+		echo($VAT_doctor);
+	}
+	echo("</p>");
 
-		// Show the received doctor
-		echo("<p><b>Doctor: </b>");
-		// Gets the VAT of the selected client
-		if(isset($_REQUEST['VAT_doctor'])) {
-			$VAT_doctor = $_REQUEST['VAT_doctor'];
-			echo("VAT: ");
-			echo($VAT_doctor);
-		}
-		echo("</p>");
+	// Show the received appointment
+	echo("<p><b>Appointment Date: </b>");
+	// Gets the VAT of the selected client
+	if(isset($_REQUEST['date_timestamp'])) {
+		$date_timestamp = $_REQUEST['date_timestamp'];
+		echo($date_timestamp);
+	}
+	// Gets the VAT of the selected client
+	if(isset($_REQUEST['time'])) {
+		$time = $_REQUEST['time'];
+		echo(" at ");
+		echo($time);
+	}
+	echo("</p>");
 
-		// Show the received appointment
-		echo("<p><b>Appointment Date: </b>");
-		// Gets the VAT of the selected client
-		if(isset($_REQUEST['date_timestamp'])) {
-			$date_timestamp = $_REQUEST['date_timestamp'];
-			echo($date_timestamp);
-		}
-		// Gets the VAT of the selected client
-		if(isset($_REQUEST['time'])) {
-			$time = $_REQUEST['time'];
-			echo(" at ");
-			echo($time);
-		}
-		echo("</p>");
+	$query = 'SELECT quadrant,number,description,measure FROM procedure_charting pc WHERE pc.date_timestamp=:date_timestamp AND pc.VAT=:VAT_doctor;';
+	$queryVariables = array();
+	$queryVariables[':VAT_doctor'] = $VAT_doctor;
+	$queryVariables[':date_timestamp'] = $date_timestamp;
+	$sql = $connection->prepare($query);
+	if(!$sql->execute($queryVariables)){
+		$info = $connection->errorInfo();
+		echo("<p>Error: {$info[2]}</p>");
+		exit();
+	}
+	$result=$sql->fetchAll();
+	if($result == 0) {
+	    $info = $sql->errorInfo();
+	    echo("<p>Error: {$info[2]}</p>");
+	    exit();
+	}
 
-		$query = 'SELECT quadrant,number,description,measure FROM procedure_charting pc WHERE pc.date_timestamp=:date_timestamp AND pc.VAT=:VAT_doctor;';
+
+	if ($sql->rowCount() == 0) {
+			//No diagnosis found
+		echo("<p>No dental procedures charting found.</p>");
+		
+		$query = 'SELECT * FROM teeth;';
 		$queryVariables = array();
-		$queryVariables[':VAT_doctor'] = $VAT_doctor;
-		$queryVariables[':date_timestamp'] = $date_timestamp;
 		$sql = $connection->prepare($query);
 		if(!$sql->execute($queryVariables)){
 			$info = $connection->errorInfo();
 			echo("<p>Error: {$info[2]}</p>");
 			exit();
 		}
+
 		$result=$sql->fetchAll();
+
 		if($result == 0) {
 		    $info = $sql->errorInfo();
 		    echo("<p>Error: {$info[2]}</p>");
 		    exit();
 		}
 
+		?>
 
-		if ($sql->rowCount() == 0) {
-				//No diagnosis found
-			echo("<p>No dental procedures charting found.</p>");
-			
-			$query = 'SELECT * FROM teeth;';
-			$queryVariables = array();
-			$sql = $connection->prepare($query);
-			if(!$sql->execute($queryVariables)){
-				$info = $connection->errorInfo();
-				echo("<p>Error: {$info[2]}</p>");
-				exit();
-			}
-
-			$result=$sql->fetchAll();
-
-			if($result == 0) {
-			    $info = $sql->errorInfo();
-			    echo("<p>Error: {$info[2]}</p>");
-			    exit();
-			}
-
-			?>
-
-			<form action='#' method='post'>
-			<p><input type="hidden" name="VAT_doctor" value="<?=$_REQUEST['VAT_doctor']?>"/></p>
-			<p><input type="hidden" name="date_timestamp"	value="<?=$_REQUEST['date_timestamp']?>"/></p>
-			<p><input type="hidden" name="Client_Name"	value="<?=$_REQUEST['Client_Name']?>"/></p>
-			<p><input type="hidden" name="VAT_client"	value="<?=$_REQUEST['VAT_client']?>"/></p>
-			<?php
-			$i = 0;
-			echo("<table border=\"1\">");
-			echo("<tr><td>Quadrant</td><td>Number</td><td>Description</td><td>Measure</td></tr>");
-			foreach ($result as $row) {
-				echo("<tr><td>");
-				echo($row['quadrant']);
-				echo("</td>");
-				echo("<td>");
-				echo($row['number']);
-				echo("</td>");
-				echo("<td>");
-				echo("<INPUT TYPE='TEXT' NAME='desc$i' SIZE='20' REQUIRED>");
-				echo("</td>");
-				echo("<td>");
-				echo("<INPUT TYPE='number' NAME='measure$i' REQUIRED>");
-				echo("</td></tr>");
-				$i=$i+1;
-			}
-			echo("</table>");
-			?>
-			<p><input type="submit" value="Submit" name="submit"/></p>
-			</form>
-
-			<?php
-				
-		} else{
-
-			echo("<table border=\"1\">");
-			echo("<tr><td>Quadrant</td><td>Number</td><td>Description</td><td>Measure</td></tr>");
-			foreach ($result as $row) {
-				echo("<tr><td>");
-				echo($row['quadrant']);
-				echo("</td>");
-				echo("<td>");
-				echo($row['number']);
-				echo("</td>");
-				echo("<td>");
-				echo($row['description']);
-				echo("</td>");
-				echo("<td>");
-				echo($row['measure']);
-				echo("</td></tr>");
-			}
-			echo("</table>");
-
+		<form action='#' method='post'>
+		<p><input type="hidden" name="VAT_doctor" value="<?=$_REQUEST['VAT_doctor']?>"/></p>
+		<p><input type="hidden" name="date_timestamp"	value="<?=$_REQUEST['date_timestamp']?>"/></p>
+		<p><input type="hidden" name="Client_Name"	value="<?=$_REQUEST['Client_Name']?>"/></p>
+		<p><input type="hidden" name="VAT_client"	value="<?=$_REQUEST['VAT_client']?>"/></p>
+		<?php
+		$i = 0;
+		echo("<table border=\"1\">");
+		echo("<tr><td>Quadrant</td><td>Number</td><td>Description</td><td>Measure</td></tr>");
+		foreach ($result as $row) {
+			echo("<tr><td>");
+			echo($row['quadrant']);
+			echo("</td>");
+			echo("<td>");
+			echo($row['number']);
+			echo("</td>");
+			echo("<td>");
+			echo("<INPUT TYPE='TEXT' NAME='desc$i' SIZE='20' REQUIRED>");
+			echo("</td>");
+			echo("<td>");
+			echo("<INPUT TYPE='number' NAME='measure$i' REQUIRED>");
+			echo("</td></tr>");
+			$i=$i+1;
 		}
+		echo("</table>");
+		?>
+		<p><input type="submit" value="Submit" name="submit"/></p>
+		</form>
+
+		<?php
+			
+	} else{
+
+		echo("<table border=\"1\">");
+		echo("<tr><td>Quadrant</td><td>Number</td><td>Description</td><td>Measure</td></tr>");
+		foreach ($result as $row) {
+			echo("<tr><td>");
+			echo($row['quadrant']);
+			echo("</td>");
+			echo("<td>");
+			echo($row['number']);
+			echo("</td>");
+			echo("<td>");
+			echo($row['description']);
+			echo("</td>");
+			echo("<td>");
+			echo($row['measure']);
+			echo("</td></tr>");
+		}
+		echo("</table>");
 
 	}
 	echo("<button onclick=\"goBack2()\">Go Back</button>");
